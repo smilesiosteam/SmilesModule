ad8f7554172f29830952019b885918f34dcd0702
Asad Ullah <asadullah.sher@systemsltd.com>
2024-02-01 16:41:41 +0500

    - changes

--- Package.swift
+++ Package.swift
@@ -19,7 +19,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "SmilesMonoRepo"),
-        .target(name: "SmilesFontsManager", dependencies: [], path: "SmilesFontsManager/Sources/"),
-        .testTarget(name: "SmilesMonoRepoTests", dependencies: ["SmilesMonoRepo"]),
+        .target(name: "SmilesFontsManager", dependencies: [], path: "SmilesFontsManager/Sources/")
     ]
 )
--- SmilesFontsManager/Package.swift
+++ SmilesFontsManager/Package.swift
@@ -19,6 +19,10 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SmilesFontsManager",
-            resources: [.process("Fonts")])
+            dependencies: [],
+            resources: [.process("Fonts")]),
+        .testTarget(
+            name: "SmilesFontsManagerTests",
+            dependencies: ["SmilesFontsManager"]),
     ]
 )
