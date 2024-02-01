ea3440dbb2d8ba35f6052dcbcbe25f19bbebdebf
Asad Ullah <asadullah.sher@systemsltd.com>
2024-02-01 18:02:41 +0500

    - changes

--- Package.swift
+++ Package.swift
@@ -10,15 +10,18 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "SmilesMonoRepo", targets: ["SmilesFontsManager"]),
+        .library(name: "SmilesMonoRepo", targets: ["SmilesMonoRepo"]),
     ],
-//    dependencies: [
-//        .package(path: "SmilesFontsManager")
-//    ],
+    dependencies: [
+        .package(path: "SmilesFontsManager")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "SmilesMonoRepo"),
-        .target(name: "SmilesFontsManager", dependencies: [], path: "SmilesFontsManager/Sources/")
+        .target(
+            name: "SmilesMonoRepo",
+            dependencies: [
+                .product(name: "SmilesFontsManager", package: "SmilesFontsManager")
+            ])
     ]
 )
