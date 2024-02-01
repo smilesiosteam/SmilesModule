589d1d8e4743182b8a1a9a7563d4ac86c21d3e0e
Asad Ullah <asadullah.sher@systemsltd.com>
2024-02-01 18:41:56 +0500

    - changes

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-
+ 
 let package = Package(
     name: "SmilesMonoRepo",
     platforms: [
