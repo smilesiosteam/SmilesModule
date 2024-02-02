ea62f74c3ca07464fbe459d9f0196dfcfd094cd3
Asad Ullah <asadullah.sher@systemsltd.com>
2024-02-02 14:31:46 +0500

    - changes

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
         .library(name: "SmilesMonoRepo",
                  targets: [
                     "SmilesFontsManager",
-                    "SmilesUtilities",
+                    //"SmilesUtilities",
                     "SmilesStorage",
-                    "SmilesLanguageManager",
+                    //"SmilesLanguageManager",
                  ]),
     ],
 //    dependencies: [
@@ -26,8 +26,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "SmilesMonoRepo"),
         .target(name: "SmilesFontsManager", dependencies: [], path: "SmilesFontsManager/Sources/SmilesFontsManager/"),
-        .target(name: "SmilesUtilities", dependencies: [], path: "SmilesUtilities/Sources/SmilesUtilities/"),
+        //.target(name: "SmilesUtilities", dependencies: [], path: "SmilesUtilities/Sources/SmilesUtilities/"),
         .target(name: "SmilesStorage", dependencies: [], path: "SmilesStorage/Sources/SmilesStorage/"),
-        .target(name: "SmilesLanguageManager", dependencies: [], path: "SmilesLanguageManager/Sources/SmilesLanguageManager/"),
+        //.target(name: "SmilesLanguageManager", dependencies: [], path: "SmilesLanguageManager/Sources/SmilesLanguageManager/"),
     ]
 )
