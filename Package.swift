0a35a95e7541035922e3035f359e7973807fc86f
Asad Ullah <asadullah.sher@systemsltd.com>
2024-02-03 23:44:21 +0500

    - changes

--- Package.swift
+++ Package.swift
@@ -9,14 +9,10 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "SmilesMonoRepo",
-                 targets: [
-                    "SmilesFontsManager",
-                    "SmilesUtilities",
-                    "SmilesStorage",
-                    "SmilesLanguageManager",
-                 ]),
+        .SmilesFontsManager,
+        .SmilesUtilities,
+        .SmilesStorage,
+        .SmilesLanguageManager,
     ],
     dependencies: [
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.8.0")),
@@ -24,19 +20,63 @@ let package = Package(
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "SmilesMonoRepo"),
-        .target(name: "SmilesFontsManager", dependencies: [], path: "SmilesFontsManager/Sources/SmilesFontsManager/"),
-        .target(name: "SmilesUtilities", 
-                dependencies: ["SmilesLanguageManager",], path: "SmilesUtilities/Sources/SmilesUtilities/"),
-        .target(name: "SmilesStorage", dependencies: [], path: "SmilesStorage/Sources/SmilesStorage/"),
-        .target(name: "SmilesLanguageManager",
-                dependencies: ["SmilesStorage",
-                               "SmilesFontsManager",
-                               "CryptoSwift",
-                               "SkeletonView",
-                               "SDWebImage"],
-                path: "SmilesLanguageManager/Sources/SmilesLanguageManager/"),
+        .SmilesMonoRepo,
+        .SmilesFontsManager,
+        .SmilesStorage,
+        .SmilesUtilities,
+        .SmilesLanguageManager,
     ]
 )
+
+
+extension String {
+    static let SmilesMonoRepo = "SmilesMonoRepo"
+    static let SmilesFontsManager = "SmilesFontsManager"
+    static let SmilesUtilities = "SmilesUtilities"
+    static let SmilesStorage = "SmilesStorage"
+    static let SmilesLanguageManager = "SmilesLanguageManager"
+
+    enum Prefixed {
+        static let SmilesFontsManager = "SmilesFontsManager"
+        static let SmilesUtilities = "SmilesUtilities"
+        static let SmilesStorage = "SmilesStorage"
+        static let SmilesLanguageManager = "SmilesLanguageManager"
+    }
+}
+
+extension Product {
+    static let SmilesFontsManager = library(name: .SmilesFontsManager, targets: [.Prefixed.SmilesFontsManager])
+    static let SmilesUtilities = library(name: .SmilesUtilities, targets: [.SmilesUtilities, .Prefixed.SmilesUtilities])
+    static let SmilesStorage = library(name: .SmilesStorage, targets: [.SmilesStorage])
+    static let SmilesLanguageManager = library(name: .SmilesLanguageManager, targets: [.SmilesLanguageManager, .Prefixed.SmilesLanguageManager])
+}
+
+extension Target {
+   
+    static let SmilesMonoRepo = target(name: .SmilesMonoRepo)
+    
+    static let SmilesStorage = target(name: .SmilesStorage, dependencies: [],
+                                           path: "SmilesStorage/Sources/SmilesStorage/")
+    
+    static let SmilesLanguageManager = target(name: .SmilesLanguageManager, 
+                                              dependencies: [.SmilesStorage],
+                                              path: "SmilesLanguageManager/Sources/SmilesLanguageManager/")
+    
+    static let SmilesFontsManager = target(name: .SmilesFontsManager, dependencies: [],
+                                           path: "SmilesFontsManager/Sources/SmilesFontsManager/")
+    
+    static let SmilesUtilities = target(name: .SmilesUtilities,
+                                        dependencies: [.SmilesFontsManager, .SmilesLanguageManager, .CryptoSwift, .SkeletonView, .SDWebImage],
+                                        path: "SmilesUtilities/Sources/SmilesUtilities/")
+}
+
+
+extension Target.Dependency {
+    static let SmilesFontsManager = byName(name: .SmilesFontsManager)
+    static let SmilesUtilities = byName(name: .SmilesUtilities)
+    static let SmilesStorage = byName(name: .SmilesStorage)
+    static let SmilesLanguageManager = byName(name: .SmilesLanguageManager)
+    static let CryptoSwift = byName(name: "CryptoSwift")
+    static let SkeletonView = byName(name: "SkeletonView")
+    static let SDWebImage = byName(name: "SDWebImage")
+}
